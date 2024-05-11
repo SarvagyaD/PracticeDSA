55b66f57cdd708c209f726e234d51926befc0ab3
SarvagyaD <76919220+SarvagyaD@users.noreply.github.com>
2024-05-11 19:14:11 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,51 @@
+import java.util.Objects;
+
+public class Employee {
+    int id;
+    String name ;
+    int age;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return name.equals(employee.name);
+    }
+
+    @Override
+    public int hashCode() {
+        //return Objects.hash(name);
+        final int prime =31;
+        int result =1;
+        result =prime * result +((name == null) ? 0 :name.hashCode());
+        return result;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+}
